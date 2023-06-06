85e6b1a7cbef55b33421b03329e9f3759fa40de6
gautamankita123 <ankitagautam6263@gmail.com>
2023-06-06 14:33:33 +0530

    manyToMany commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const Cart = require('./models/cart-item');
 
 const app = express();
 
@@ -35,6 +37,12 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
+
 
 sequelize
   //.sync({ force: true })
@@ -51,10 +59,16 @@ sequelize
     return user;
   })
   .then(user => {
-    console.log(user);
-    app.listen(3000);
-
+    //console.log(user);
+    return user.createCart();
   })
-  .catch(err => {
-    console.log(err);
-  });
+  .then(cart => {
+    app.listen(3000);
+  })
+//
+app.listen(3000);
+
+
+  .catch (err => {
+  console.log(err);
+});
